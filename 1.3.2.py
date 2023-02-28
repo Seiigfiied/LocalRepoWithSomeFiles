1cbef9353b9e78ffc49dcf2223ebafbeb23dd463
Max <marvingaye23@mail.ru>
2023-02-28 19:04:05 +0300

    Some files

---
+++ 1.3.2.py
@@ -0,0 +1,8 @@
+def multiply_by_12(x):
+    x1 = x + x
+    x2 = x1 + x1
+    x3 = x2 + x2
+    x4 = x3 + x3
+    return x4
+
+print(multiply_by_12(2))
---
+++ 1.3.3.py
@@ -0,0 +1,10 @@
+def multiply_by_15(x):
+    x1 = x + x
+    x2 = x1 + x1
+    x3 = x2 + x
+    x4 = x3 - x
+    return x4
+
+
+
+print(multiply_by_15(4))
